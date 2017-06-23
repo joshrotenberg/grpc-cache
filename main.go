eb1d83e4b4a1d28c8c739affb9ac845b918b93aa
josh rotenberg <joshrotenberg@gmail.com>
2017-06-22 17:08:00 -0700

    basic main functionality

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/joshrotenberg/grpc-cache/server"
+)
+
+var (
+	serverAddr      string
+	cacheMaxEntries int
+)
+
+func init() {
+	flag.StringVar(&serverAddr, "addr", "", "host:port to listen on")
+	flag.IntVar(&cacheMaxEntries, "maxEntries", 0, "maxiumum cache entries")
+	flag.Parse()
+}
 func main() {
+	sigs := make(chan os.Signal, 1)
 
-	log.Println("cool")
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	s, err := server.New(serverAddr, cacheMaxEntries)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start()
+	<-sigs
+	s.Stop()
 }
